644f46bd5141e4278da0c539bfdcc0bb1138d1b6
Val++le <v.kummert@drmnt.de>
2020-08-27 08:47:29 +0200

    Initial Canvas Test

---
+++ script.js
@@ -0,0 +1,69 @@
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+
+canvas.width = canvas.height = 300;
+
+var x = 150,
+    y = 150,
+    velY = 0,
+    velX = 0,
+    speed = 2,
+    friction = 0.7,
+    keys = [];
+
+function update() {
+    requestAnimationFrame(update);
+    
+    if (keys[38]) {
+        if (velY > -speed) {
+            velY--;
+        }
+    }
+    
+    if (keys[40]) {
+        if (velY < speed) {
+            velY++;
+        }
+    }
+    if (keys[39]) {
+        if (velX < speed) {
+            velX++;
+        }
+    }
+    if (keys[37]) {
+        if (velX > -speed) {
+            velX--;
+        }
+    }
+
+    velY *= friction;
+    y += velY;
+    velX *= friction;
+    x += velX;
+
+    if (x >= 295) {
+        x = 295;
+    } else if (x <= 5) {
+        x = 5;
+    }
+
+    if (y > 295) {
+        y = 295;
+    } else if (y <= 5) {
+        y = 5;
+    }
+
+    ctx.clearRect(0, 0, 300, 300);
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+update();
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
