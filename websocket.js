a02f58111e0d3508bc9d73753227d23ded9090e5
valplusplusle <v.kummert@drmnt.de>
2020-08-30 17:01:16 +0200

    add websocket test script

---
+++ websocket.js
@@ -0,0 +1,18 @@
+// Let us open a web socket
+var ws = new WebSocket("ws://localhost:9998/echo");
+
+ws.onopen = function() {
+    // Web Socket is connected, send data using send()
+    ws.send("Message to send");
+    alert("Message is sent...");
+};
+
+ws.onmessage = function (evt) { 
+    var received_msg = evt.data;
+    alert("Message is received...");
+};
+
+ws.onclose = function() {   
+    // websocket is closed.
+    alert("Connection is closed..."); 
+};
