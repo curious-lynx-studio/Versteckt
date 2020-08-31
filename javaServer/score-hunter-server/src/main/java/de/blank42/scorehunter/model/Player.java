0452c35dcbf9b3f7cb810a2fae80fbfa89abcc3b
crissi98 <christian.radow@online.de>
2020-08-31 17:32:22 +0200

    Added Quarkus-Server for handling of websocket connections

---
+++ javaServer/score-hunter-server/src/main/java/de/blank42/scorehunter/model/Player.java
@@ -0,0 +1,78 @@
+package de.blank42.scorehunter.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.websocket.Session;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+
+public class Player {
+
+    static long activeTimeout = 60L;
+
+    private int id;
+
+    @JsonIgnore //ignored because not needed in JSON
+    private Session wsSession;
+
+    private List<Position> positions;
+
+    @JsonIgnore
+    private LocalTime lastUpdated;
+
+    public Player() {
+        positions = List.of();
+    }
+
+    public Player(int id, Session wsSession) {
+        this();
+        this.id = id;
+        this.wsSession = wsSession;
+        lastUpdated = LocalTime.now();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPositions(List<Position> positions) {
+        this.positions = positions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
+    public void updatePositions(Player newData) {
+        this.positions = newData.getPositions();
+        lastUpdated = LocalTime.now();
+    }
+
+    @JsonIgnore
+    public boolean isInactive() {
+        return Duration.between(lastUpdated, LocalTime.now()).toSeconds() > activeTimeout;
+    }
+
+    public void sendData(String toSend) {
+        wsSession.getAsyncRemote().sendText(toSend);
+    }
+
+    public void closeConnection() {
+        try {
+            wsSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isPlayersSession(Session otherSession) {
+        return wsSession.equals(otherSession);
+    }
+
+}
---
+++ javaServer/score-hunter-server/src/main/java/de/blank42/scorehunter/websocket/MessageSocket.java
@@ -0,0 +1,86 @@
+package de.blank42.scorehunter.websocket;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.blank42.scorehunter.model.Player;
+import io.quarkus.scheduler.Scheduled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@ApplicationScoped
+@ServerEndpoint(value = "/positions")
+public class MessageSocket {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageSocket.class);
+
+    private static int idGenerator = 1;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    Map<Integer, Player> players;
+
+    @PostConstruct
+    void init() {
+        players = new ConcurrentHashMap<>();
+    }
+
+    @OnOpen
+    public void openSession(Session session) {
+        int id = idGenerator;
+        idGenerator++;
+        players.put(id, new Player(id, session));
+        LOG.info("New player connected. Setting id {}", id);
+        session.getAsyncRemote().sendText("id=" + id);
+    }
+
+    @OnMessage
+    public void notifyUpdate(String messageRcv) throws JsonProcessingException {
+        Player playerUpdate = MAPPER.readValue(messageRcv, Player.class);
+        LOG.info("Received data from player {}. Notifying others", playerUpdate.getId());
+        Player playerToUpdate = players.get(playerUpdate.getId());
+        playerToUpdate.updatePositions(playerUpdate);
+        String messageToSend = MAPPER.writeValueAsString(players.values());
+        players.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey() != playerUpdate.getId())
+                .map(Map.Entry::getValue)
+                .forEach(player -> player.sendData(messageToSend));
+    }
+
+    @OnClose
+    public void closeSession(Session session) {
+        players.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().isPlayersSession(session))
+                .findFirst()
+                .ifPresent(playerToRemove -> {
+                    players.remove(playerToRemove.getKey());
+                    LOG.info("player {} removed", playerToRemove.getKey());
+                });
+    }
+
+    @Scheduled(every = "10s", delay = 5, delayUnit = TimeUnit.SECONDS)
+    public void removeUnusedSessions() {
+        players.values()
+                .stream()
+                .filter(Player::isInactive)
+                .forEach(Player::closeConnection);
+    }
+
+    @PreDestroy
+    void shutdown() {
+        players.values().forEach(Player::closeConnection);
+    }
+}
