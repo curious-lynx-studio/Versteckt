c9ec957328915cbe4d03d7693b889ec9d9563f09
valplusplusle <v.kummert@drmnt.de>
2020-09-01 20:06:35 +0200

    add other clients

--- websocket.js
+++ websocket.js
@@ -8,7 +8,16 @@ webSocket.onmessage = (message) => {
         id = message.data.substring(3);
         var positionSendLoop = setInterval(sendData, 100);
     } else {
-        console.log("Writing data")
+        const obj = JSON.parse(message.data);
+        obj.forEach(user => {
+            if(user.id != id) {
+                if(document.getElementById(user.id)){
+                    updatePlayerObj(user);
+                } else {
+                    createPlayerObj(user);
+                }
+            }
+        });
     }
 }
 
@@ -24,6 +33,30 @@ function getNetworkJson(received_msg) {
 function sendData() {
     var x = document.getElementById("player").style.left;
     var y = document.getElementById("player").style.top;
+    x = x.substring(0, x.length - 2);
+    y = y.substring(0, y.length - 2);
     let data = {id: id, positions: [{x: x, y: y}]};
     webSocket.send(JSON.stringify(data));
+}
+
+function updatePlayerObj(user) {
+    const x = user.positions[0].x + "px";
+    const y = user.positions[0].y + "px";
+    console.log(x,y)
+    const otherClient = document.getElementById(user.id);
+    otherClient.style.left = x;
+    otherClient.style.top = y;
+}
+
+function createPlayerObj(user) {
+    const x = user.positions[0].x + "px";
+    const y = user.positions[0].y + "px";
+    console.log(x,y)
+    const otherClient = document.createElement("DIV");
+    otherClient.className = "otherClient";
+    otherClient.id = user.id;
+    console.log(user.positions[0].x)
+    otherClient.style.left = user.positions[0].x + "px";
+    otherClient.style.top = user.positions[0].y + "px";
+    document.getElementById("gameArea").appendChild(otherClient);
 }
