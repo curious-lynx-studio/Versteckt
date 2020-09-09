ac376e2edd1b1c89453cd33faffdd8f072b12a85
valplusplusle <v.kummert@drmnt.de>
2020-09-09 23:22:57 +0200

    add boss as test object

---
+++ gameFiles/bossUrug.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+    spawnTheBoss();
+}) 
+
+function spawnTheBoss() {
+    var bossUrug = document.createElement("div");
+    bossUrug.id = "bossUrug";
+    bossUrug.className = "bossUrug";
+    bossUrug.style.left = "430px";
+    bossUrug.style.top = "190px";
+    document.getElementById("gameArea").appendChild(bossUrug);
+}
