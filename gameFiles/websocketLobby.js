657ff2b0871e9856244d473e629b7967754b7786
valplusplusle <v.kummert@drmnt.de>
2020-09-13 00:38:26 +0200

    add lobby ready button

--- gameFiles/websocketLobby.js
+++ gameFiles/websocketLobby.js
@@ -1,4 +1,5 @@
 var webSocket = new WebSocket("ws://blank42.de:9998/lobbyList");
+var lobbyWebSocket;
 
 webSocket.onmessage = (message) => {
     const obj = JSON.parse(message.data);
@@ -22,17 +23,40 @@ webSocket.onclose = function() {
 function connectToLobby(lobbyUrl) {
     webSocket.close();
     document.getElementById("body").innerHTML = "";
+    setStdUserName();
     var playerName = localStorage.getItem('playerName');
-    var password = "";
-    var lobbyWebSocket = new WebSocket(lobbyUrl);
+    var password = "pw";
+    lobbyWebSocket = new WebSocket(lobbyUrl);
 
     lobbyWebSocket.onopen = function(e) {
         let data = {playerName: playerName, password: password};
         lobbyWebSocket.send(JSON.stringify(data));
+        document.getElementById("body").innerHTML = `
+            <div class="container">
+                <div class="jumbotron mainJumbotron">
+                    <h4>Wait for all players to be ready.</h4>
+                    <br><br>
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only"></span>
+                    </div>
+                    <br><br><br>
+                    <div class="btn btn-primary" onclick="sendReady()">Ready?<div>
+                </div>
+            </div> 
+        `;
+    };
+
+    lobbyWebSocket.onclose = function() {   
+        console.log("Connection is closed..."); 
     };
 
     lobbyWebSocket.onmessage = (message) => {
         const obj = JSON.parse(message.data);
         console.log(obj);
     }
+}
+
+function sendReady() {
+    var playerName = localStorage.getItem('playerName');
+    lobbyWebSocket.send(playerName+':ready:false');
 }
