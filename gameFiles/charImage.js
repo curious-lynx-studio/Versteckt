830b0cc89fbcb89c3e989cb1d6dfd85060641421
valplusplusle <v.kummert@drmnt.de>
2020-09-24 20:32:46 +0200

    add new character assets

--- gameFiles/charImage.js
+++ gameFiles/charImage.js
@@ -3,15 +3,15 @@ var char = document.getElementById("player");
 
 function logKey(e) {
     if (e.keyCode === 97) {
-        char.style.backgroundImage = 'url(gameAssets/Character/Char01_Left.png)';
+        char.style.backgroundImage = 'url(gameAssets/Character/blue_left.png)';
     }
     if (e.keyCode === 100) {
-        char.style.backgroundImage = 'url(gameAssets/Character/Char01_Right.png)';
+        char.style.backgroundImage = 'url(gameAssets/Character/blue_right.png)';
     }
     if (e.keyCode === 119) {
-        char.style.backgroundImage = 'url(gameAssets/Character/Char01_Back.png)';
+        char.style.backgroundImage = 'url(gameAssets/Character/blue_up.png)';
     }
     if (e.keyCode === 115) {
-        char.style.backgroundImage = 'url(gameAssets/Character/Char01_Front.png)';
+        char.style.backgroundImage = 'url(gameAssets/Character/blue_down.png)';
     }
 }
