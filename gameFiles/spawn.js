e3271caa91febb6d6991fc152f4133b7fbd12502
valplusplusle <v.kummert@drmnt.de>
2020-09-25 00:36:16 +0200

    add character spawn points

---
+++ gameFiles/spawn.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){ 
+    setPlayerSpawn();
+});
+
+function setPlayerSpawn() {
+    let playerModel = localStorage.getItem('playerModel');
+    var char = document.getElementById("player");
+    if (playerModel == '0') {
+        char.style.top = "250px"
+        char.style.left = "10px"
+    }
+    if (playerModel == '1') {
+        char.style.top = "250px"
+        char.style.left = "900px"
+    }
+    if (playerModel == '2') {
+        char.style.top = "0px"
+        char.style.left = "460px"
+    }
+    if (playerModel == '3') {
+        char.style.top = "545px"
+        char.style.left = "460px"
+    }
+}
