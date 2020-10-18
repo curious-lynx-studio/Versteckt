2075625481b0ee8405264da9ac6bfd34255cf6e4
valplusplusle <v.kummert@drmnt.de>
2020-10-18 17:48:00 +0200

    add movement endings

--- gameFiles/movement.js
+++ gameFiles/movement.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
         
         // update position
         // left/right
-        if (keys[keyCodes.left]) {
+        if (keys[keyCodes.left] && canMoveLeft()) {
             x -= 1;
-        } else if (keys[keyCodes.right]) {
+        } else if (keys[keyCodes.right] && canMoveRight()) {
             x += 1;
         }
         // up/down
-        if (keys[keyCodes.up]) {
+        if (keys[keyCodes.up] && canMoveUp()) {
             y -= 1;
-        } else if (keys[keyCodes.down]) {
+        } else if (keys[keyCodes.down] && canMoveDown()) {
             y += 1;
         }
         // set div position
@@ -34,3 +34,39 @@ window.addEventListener('keydown', function (evt) {
 window.addEventListener('keyup', function (evt) {
     keys[evt.code] = false;
 });
+
+function canMoveLeft() {
+    x = parseInt(p1.style.left, 10);
+    if (x <= 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function canMoveRight() {
+    x = parseInt(p1.style.left, 10);
+    if (x >= 920) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function canMoveUp() {
+    y = parseInt(p1.style.top, 10);
+    if (y <= 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function canMoveDown() {
+    y = parseInt(p1.style.top, 10);
+    if (y >= 550) {
+        return false;
+    } else {
+        return true;
+    }
+}
