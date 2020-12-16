f26333dfadabf59280a0627761e5f289ff97473a
Valentin Kummert <v.kummert@drmnt.de>
2020-12-16 01:42:29 +0100

    add some game assets, change ui on startpage

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
 
   win.loadFile('index.html')
   win.removeMenu();
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
