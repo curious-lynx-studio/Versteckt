116b5e6266a4b06e391038137c99332d1c298aa2
Valentin Kummert <v.kummert@drmnt.de>
2021-01-30 23:31:46 +0100

    add a lobby online counter

--- gameFiles/lobby/getLobbys.js
+++ gameFiles/lobby/getLobbys.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    getLobbyCount();
+});
+
 function joinGame() {
     var code = document.getElementById('gameCode').value
     setPlayerName();
@@ -6,8 +10,25 @@ function joinGame() {
 }
 
 function getLobbyCount() {
-    postData('https://blank42.de:3033/getOnlineLobbys')
+    getData('https://blank42.de:3033/getOnlineLobbys', {})
     .then(data => {
-        console.log(data);
+        document.getElementById('lobbysOnline').innerHTML = "Lobbys Online: "+data;
     });
+}
+
+async function getData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
 }
