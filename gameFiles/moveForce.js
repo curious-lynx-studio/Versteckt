3d3d97cc71284a5beff7ec43af4cdfee83b888de
Valentin Kummert <v.kummert@drmnt.de>
2021-07-04 01:22:22 +0200

    show a cage for the last moved point where you have to move out

--- gameFiles/moveForce.js
+++ gameFiles/moveForce.js
@@ -18,15 +18,21 @@ function startNotMovedTimer() {
         let movedY = false;
         if (!(nowY > lastY-50 && nowY < lastY+50)) {movedY = true}
 
+        if(lastX == 0 || lastY == 0) {movedX = true; movedY = true;}
+        
         if (movedY || movedX || playerModel == 'deadPlayer' || playerModel == 'deadPlayer-left' || playerModel == 'deadPlayer-right'){
             lastX = nowX;
             lastY = nowY;
             document.getElementById("lastTimeMoved").innerHTML = 25;
+            document.getElementById('moveForceCage').style.visibility = 'hidden';
             stopNotMovedTimer();
         } else {
             sec--;
             if (sec == 8) {
                 moveMessage();
+            }
+            if (sec == 10) {
+                showCage(lastX, lastY);
             }
             if (sec == 5 || sec == 4 || sec == 3 || sec == 2 || sec == 1) {
                 timePlay();
@@ -47,4 +53,10 @@ function stopNotMovedTimer() {
 function resetNotMoveTimer() {
     document.getElementById("lastTimeMoved").innerHTML = '';
     clearInterval(window.notMovedTimer);
+}
+
+function showCage(x, y) {
+    document.getElementById('moveForceCage').style.top = y.toString()+'px';
+    document.getElementById('moveForceCage').style.left = x.toString()+'px';
+    document.getElementById('moveForceCage').style.visibility = 'visible';
 }
